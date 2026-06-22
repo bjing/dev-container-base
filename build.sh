327e764ac2531648c8e2c0c3ca5fcfb6fc21bb74
Brian Jing <brian.jing.dev@gmail.com>
2026-06-22 21:12:42 +1000

    Update build script to also push to dockerhub

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-docker build -t dev-container-base:latest .
+set -e
+
+IMAGE_NAME="bjing/dev-container-base"
+DATE_TAG="$(date +%Y%m%d)"
+
+docker build \
+  -t "${IMAGE_NAME}:${DATE_TAG}" \
+  -t "${IMAGE_NAME}:latest" \
+  .
+
+docker push "${IMAGE_NAME}:${DATE_TAG}"
+docker push "${IMAGE_NAME}:latest"
